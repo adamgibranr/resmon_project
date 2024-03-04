aabdae856b89d7aab85611699a1425eb176473a7
adamgibranr <135125897+adamgibranr@users.noreply.github.com>
2024-03-04 13:35:54 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -32,7 +32,19 @@
     ?>
     <h4>Informasi CPU: <?php echo $cpu_info; ?></h4>
     <p>Physical cores: <?php echo $decoded_cpu_info->physical_cores; ?></p>
+    <p>Total cores: <?php echo $decoded_cpu_info->total_cores; ?></p>
+    <p>CPU usage per core: <?php foreach ($decoded_cpu_info->cpu_usage_per_core as $core => $usage) {
+        echo "Core $core: $usage%\n";
+    } ?>
+    </p>
+    <p>Processor speed: <?php echo $decoded_cpu_info->processor_speed; ?></p>
+    
+    <p>Total CPU usage: <?php echo $decoded_cpu_info->total_cpu_usage; ?></p>
     <h4>Informasi Memori: <?php echo $memory_info; ?></h4>
+    <p>Total memory: <?php echo $decoded_memory_info->total_memory; ?></p>
+    <p>Available memory: <?php echo $decoded_memory_info->available_memory; ?></p>
+    <p>Used memory: <?php echo $decoded_memory_info->used_memory; ?></p>
+    <p>Memory percentage: <?php echo $decoded_memory_info->memory_percentage; ?></p>
 
     <p>This is a static HTML content.</p>
 </body>
